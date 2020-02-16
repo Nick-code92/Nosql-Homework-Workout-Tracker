40701af52e8d2059c4803d813709805293fc0f2d
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-02-15 19:47:38 -0700

    updated heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use(express.static("public"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://NIck:Nick9245@ds121603.mlab.com:21603/heroku_qdqtzbst", { useMongoClient: true })
 
 
 app.use(require("./routes/apiroutes.js"))
